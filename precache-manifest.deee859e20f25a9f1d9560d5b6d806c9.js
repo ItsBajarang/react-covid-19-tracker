f8380815ac6cdba7b3ebaedfcf6f9644961fea26
Bajarang Jadhav <bajarangjadhav2019@gmail.com>
2020-10-04 11:29:31 +0530

    Updates

---
+++ precache-manifest.deee859e20f25a9f1d9560d5b6d806c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a74f681bf6ccfab9833b09219ab5ada",
+    "url": "/react-covid-19-tracker/index.html"
+  },
+  {
+    "revision": "096fb9665dfa148d14e0",
+    "url": "/react-covid-19-tracker/static/css/main.455fd45a.chunk.css"
+  },
+  {
+    "revision": "61d507783ff621c95d68",
+    "url": "/react-covid-19-tracker/static/js/2.84d95e66.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid-19-tracker/static/js/2.84d95e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096fb9665dfa148d14e0",
+    "url": "/react-covid-19-tracker/static/js/main.eddabc6b.chunk.js"
+  },
+  {
+    "revision": "af54740a6f3042db9bb7",
+    "url": "/react-covid-19-tracker/static/js/runtime-main.c0023f9b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
